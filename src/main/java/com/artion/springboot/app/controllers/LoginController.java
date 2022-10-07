63b941174896067ca3e2d12242d9f9b2239dff87
josexrg <josexrg@hotmail.com>
2022-10-07 13:41:02 -0500

    login and logout added

--- src/main/java/com/artion/springboot/app/controllers/LoginController.java
+++ src/main/java/com/artion/springboot/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.artion.springboot.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,12 +12,20 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash){
+    public String login(@RequestParam(value = "error", required = false) String error,@RequestParam(value = "logout", required = false) String logout, Model model, Principal principal, RedirectAttributes flash){
 
         // Ha habido un inicio de seccion previamente?
         if (principal != null){
             flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
             return "redirect:/";
+        }
+
+        if (error != null){
+            model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo!");
+        }
+
+        if (logout != null){
+            model.addAttribute("success", "Se ha cerrado la sesión correctamente!");
         }
 
         return "login";
