77d423b7e4014bb7c2b6af38d1186931bb01130b
josexrg <josexrg@hotmail.com>
2022-10-12 15:35:22 -0500

    Multilanguage es-en-de added

--- src/main/java/com/artion/springboot/app/MvcConfig.java
+++ src/main/java/com/artion/springboot/app/MvcConfig.java
@@ -5,11 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.nio.file.Paths;
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -34,5 +39,27 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Implementacion Multi-idiomas
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        return localeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
--- src/main/java/com/artion/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/artion/springboot/app/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import com.artion.springboot.app.util.paginator.PageRender;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -34,6 +35,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 
 @Controller
@@ -47,6 +49,9 @@ public class ClienteController {
 
     @Autowired
     private IUploadFileService uploadFileService;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Secured({"ROLE_USER"})
     @GetMapping(value = "/uploads/{filename:.+}")
@@ -84,7 +89,8 @@ public class ClienteController {
 
     @RequestMapping(value={"/listar", "/"}, method = RequestMethod.GET)
     public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, Authentication authentication,
-                         HttpServletRequest request){
+                         HttpServletRequest request,
+                         Locale locale){
 
         if (authentication != null){
             logger.info("Hola usuario autenticado, tu username es: ".concat(authentication.getName()));
@@ -121,7 +127,7 @@ public class ClienteController {
 
         PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 
-        model.addAttribute("titulo", "Listado de clientes");
+        model.addAttribute("titulo", messageSource.getMessage("text.cliente.listar.titulo", null, locale));
         model.addAttribute("clientes", clientes);
         model.addAttribute("page", pageRender);
 
