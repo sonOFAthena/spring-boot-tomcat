a83434e7edef2b44214b8e51b154705dca569558
josexrg <josexrg@hotmail.com>
2022-10-23 14:34:22 -0500

    excel export added

---
+++ src/main/java/com/artion/springboot/app/view/csv/ClienteCsvView.java
@@ -0,0 +1,45 @@
+package com.artion.springboot.app.view.csv;
+
+import com.artion.springboot.app.models.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
+import java.util.Map;
+
+@Component("listar")
+public class ClienteCsvView extends AbstractView {
+
+    public ClienteCsvView() {
+        setContentType("text/csv");
+    }
+
+    @Override
+    protected boolean generatesDownloadContent() {
+        return true;
+    }
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
+        response.setContentType(getContentType());
+
+        Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+
+        CsvBeanWriter beanWriter = new CsvBeanWriter( response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] header = {"id", "nombre", "apellido", "email" , "createAt"};
+        beanWriter.writeHeader(header);
+
+        for (Cliente cliente: clientes){
+            beanWriter.write(cliente, header);
+        }
+
+        beanWriter.close();
+    }
+}
